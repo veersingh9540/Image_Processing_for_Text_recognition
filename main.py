35563ad55d31c46bea8916251cbdb2f4428cf88c
sukhveer singh <44468231+veersingh9540@users.noreply.github.com>
2019-09-25 11:13:27 +0530

    Update main.py
    Changed the UI now it can be used easily 
    and added sqlite3 database for saving the data 
    and used that to search the data as well

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 import tkinter as tk
 import datetime 
-from csv import DictWriter
 from tkinter import ttk
 import cv2
-#import subprocess
-from PIL import ImageTk, Image
 import pytesseract
-import numpy as np
-#from tkinter import *
-#import time
 import sys
-#import PIL.Image
-#import numpy as np
-import pytesseract
-from pytesseract import image_to_string
 import os
-#from tkinter import filedialog
-#import tkFileDialog
-
-
+import sqlite3
+from PIL import Image
+#import search
 WIDTH = 1500
-HEIGHT =1800
+HEIGHT =1500
 root = tk.Tk()
 root.title('TOLLNITIAN')
 canvas = tk.Canvas(root , height= WIDTH,width=HEIGHT,bg='#ffffff')
@@ -33,10 +22,101 @@ def restart_program():
 def load():   
     python = sys.executable
     os.execl(python, python, * sys.argv)
+    showImage()
+
 def abt_us():
     tk.messagebox.showinfo("Developed By","Sukhveer Singh")
 def git_lnk():
-    tk.messagebox.showinfo("GITHUB","github.com/veersingh9540 give LIKE and SHARE")  
+    tk.messagebox.showinfo("GITHUB","github.com/veersingh9540 give LIKE and SHARE")      
+#open a new window on click  search
+def Search():
+    newWin = tk.Toplevel(root)
+    canvas = tk.Canvas(newWin , height= 1000,width=1000,bg='#ffffff')
+    canvas.pack()
+    #search frame 
+    search_frame= tk.Frame(newWin , bg='#b8f5ca',bd=5)
+    search_frame.place(relx=0.05,rely= 0.07,relwidth=0.5,relheight=0.90)
+    #UI making
+    vehiclefont = ('times', 20 , 'bold')
+    vehicle_label = tk.Label(search_frame,text="VEHICLE NO...",font=vehiclefont ,bg='#d4fcdf')
+    vehicle_label.place( rely=0.02 ,relwidth=1, relheight=0.05  )      
+    entryN_var = tk.StringVar()
+    vehicle_entry = tk.Entry(search_frame,text=entryN_var, textvariable= entryN_var, justify = 'center')
+    vehicle_entry.place(rely=0.075, relwidth=1 , relheight=0.05 , )    
+    vehicle_entry.focus()
+    
+    place_label = tk.Label(search_frame,text="PLACE",font=vehiclefont ,bg='#d4fcdf')
+    place_label.place( rely=0.150 ,relwidth=1, relheight=0.05  )      
+    place_var = tk.StringVar()
+    place_entry = tk.Entry(search_frame,text=place_var, textvariable= place_var, justify = 'center')
+    place_entry.place(rely=0.205, relwidth=1 , relheight=0.05 , )    
+     
+    
+    type_label = tk.Label(search_frame,text="TYPE OF TRAVEL",font=vehiclefont ,bg='#d4fcdf')
+    type_label.place( rely=0.280 ,relwidth=1, relheight=0.05  )      
+    type_var = tk.StringVar()
+    type_entry = tk.Entry(search_frame,text=type_var, textvariable= type_var, justify = 'center')
+    type_entry.place(rely=0.335, relwidth=1 , relheight=0.05 , )    
+    
+    
+    vehiclehl_label = tk.Label(search_frame,text="TYPE OF VEHICLE",font=vehiclefont ,bg='#d4fcdf')
+    vehiclehl_label.place( rely=0.41 ,relwidth=1, relheight=0.05  )      
+    entryhl_var = tk.StringVar()
+    vehiclehl_entry = tk.Entry(search_frame,text=entryhl_var, textvariable= entryhl_var, justify = 'center')
+    vehiclehl_entry.place(rely=0.465, relwidth=1 , relheight=0.05 , )    
+   
+    
+    vehicleso_label = tk.Label(search_frame,text="VEHICLE REGISTRATION",font=vehiclefont ,bg='#d4fcdf')
+    vehicleso_label.place( rely=0.54 ,relwidth=1, relheight=0.05  )      
+    entryso_var = tk.StringVar()
+    vehicleso_entry = tk.Entry(search_frame,text=entryso_var, textvariable= entryso_var, justify = 'center')
+    vehicleso_entry.place(rely=0.595, relwidth=1 , relheight=0.05 , )    
+    
+    
+    donation_label = tk.Label(search_frame,text="DONATED ??",font=vehiclefont ,bg='#d4fcdf')
+    donation_label.place( rely=0.67 ,relwidth=1, relheight=0.05  )      
+    donation_var = tk.StringVar()
+    donation_entry = tk.Entry(search_frame,text=donation_var, textvariable= donation_var, justify = 'center')
+    donation_entry.place(rely=0.725, relwidth=1 , relheight=0.05 , )    
+     
+    
+    dnt_label = tk.Label(search_frame,text="DATE AND TIME",font=vehiclefont ,bg='#d4fcdf')
+    dnt_label.place( rely=0.8 ,relwidth=1, relheight=0.05  )      
+    dnt_var = tk.StringVar()
+    dnt_entry = tk.Entry(search_frame,text=dnt_var, textvariable= dnt_var, justify = 'center')
+    dnt_entry.place(rely=0.855, relwidth=1 , relheight=0.05 , )    
+    
+    #frame for image
+    frameS= tk.Frame(newWin , bg='#000000',bd=5)
+    frameS.place(relx=0.57,rely=0.30,relwidth=0.39,relheight=0.39)
+   
+    
+    #database search
+    def database_search():
+        global SLi
+        Connect = sqlite3.connect('Toll_data.db')
+        c = Connect.cursor()
+        c.execute("SELECT * FROM Toll WHERE Vehicle_No='%s'"%entryN_var.get())
+        data=c.fetchone()
+        place_var.set(data[1])
+        type_var.set(data[2])
+        entryhl_var.set(data[3])
+        entryso_var.set(data[4])
+        donation_var.set(data[5])
+        dnt_var.set(data[6])
+        imageSL = tk.PhotoImage(Image.open(data[7]))
+        SLi= imageSL.subsample(1,2)
+         #label for image
+        cam_canvasSL= tk.Label(frameS ,width=50,height=50, image=SLi)
+        cam_canvasSL.place( relwidth=1,relheight=1)
+        Connect.commit()
+        Connect.close()
+    
+    submit_button = tk.Button(search_frame, text='SUBMIT', command =database_search)
+    submit_button.place(relx= 0.4, rely= 0.95,relheight=0.05 , relwidth=0.2 )
+    
+
+   
 menubar = tk.Menu(root)
 root.config(menu=menubar)
 # create a pulldown menu, and add it to the menu bar
@@ -48,7 +128,8 @@ filemenu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 
 filemenu2 = tk.Menu(menubar, tearoff=0)
-filemenu2.add_command(label="About Us",command=abt_us)
+filemenu2.add_command(label="About Us",command =abt_us )
+filemenu2.add_command(label="SEARCH VEHICLE DATA",command= Search)
 filemenu2.add_separator()
 filemenu2.add_command(label="Git hub Link", command=git_lnk)
 menubar.add_cascade(label="Help", menu=filemenu2)
@@ -126,14 +207,14 @@ dnt_var.set(now.strftime("%A-%d-%B-%Y %X"))
 dnt_msg.place(rely= 0.76,relwidth= 1,relheight= 0.1)
 
 #frame for camera FRAME 2
-frame2= tk.Frame(root , bg='#80c1ff',bd=5)
+frame2= tk.Frame(root , bg='#000000',bd=5)
 frame2.place(relx=0.50,rely=0.07,relwidth=0.39,relheight=0.39)
 #label for image 
 cam_canvas= tk.Label(frame2 ,width=50,height=50 )
 cam_canvas.place(relx=0, rely=0.070, relwidth=1,relheight=0.81)
 
 #LABEL for the camera 
-auto_take = tk.Label(frame2,text='AI CAMERA Demonstration')
+auto_take = tk.Label(frame2,text='AI CAMERA ')
 auto_take.place(relx=0 ,rely= 0.015, relwidth= 1, relheight=0.05)
 #camera code
 def web():
@@ -156,17 +237,15 @@ def web():
         #img_name = "opencv_frame_{}.png".format(img_counter)
         #image storing
         cv2.imwrite('img_name.png', frame)
-      
    capture.release()
    cv2.destroyAllWindows()
-   
+   threshold()
 background_image = tk.PhotoImage(file="img_name.png")   
 tmi= background_image.subsample(1,2)
     
 #thresholding function
 def threshold():
     
-    cam_canvas.config(image=tmi2)
     image1 = cv2.imread('img_name.png')
     img = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY) 
     thresh2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
@@ -182,26 +261,15 @@ def threshold():
 background_image2 = tk.PhotoImage(file="thres_img_name.png")
 tmi2=background_image2.subsample(1,2)
 
-    
-
-#image view 
-#image_list=[tmi,tmi2]
 #image for the camera
 def showImage():
     cam_canvas.config(image=tmi)
-    
+#button for TASKS frame2
+image_show = tk.Button(frame2,text='SHOW THIS IMAGE' ,command= showImage)
+image_show.place(relx= 0.3, rely= 0.9,relheight=0.08 , relwidth=0.4 )
 
-#button for TASKS
-snap = tk.Button(frame2,text='CAMERA' , command=web)
-snap.place(relx= 0.15, rely= 0.9,relheight=0.08 , relwidth=0.2 )
-
-image_show = tk.Button(frame2,text='SHOW' ,command= showImage)
-image_show.place(relx= 0.4, rely= 0.9,relheight=0.08 , relwidth=0.2 )
-  
-thres = tk.Button(frame2,text='image_correct', command =threshold )
-thres.place(relx= 0.65, rely= 0.9,relheight=0.08 , relwidth=0.2 )
 #frame for CURRENT image
-frame3= tk.Frame(root , bg='#80c1ff',bd=5)
+frame3= tk.Frame(root , bg='#000000',bd=5)
 frame3.place(relx=0.50,rely=0.50,relwidth=0.39,relheight=0.39)
 
 image2 = tk.PhotoImage(file="thres_img_name.png")
@@ -212,28 +280,25 @@ i3=image3.subsample(1,2)
 def cur_thres():
     
     cur_canvas.config(image=i2)
-    
-def cur_image():
-    #image1 = ImageTk.PhotoImage(pil_image)
-    cur_canvas.config(image=i3)
 
+def hello():
+    threshold()
+    cur_thres()
 #label for image
 cur_canvas= tk.Label(frame3 ,width=50,height=50 )
 cur_canvas.place(relx=0, rely=0.070, relwidth=1,relheight=0.81)
 #button for image SHOW 
-cur_thres = tk.Button(frame3,text='Image_Correct' ,command= cur_thres)
-cur_thres.place(relx= 0.65, rely= 0.9,relheight=0.08 , relwidth=0.2 )
+cur_thres_button = tk.Button(frame3,text='PROCESS THIS IMAGE' ,command= hello)
+cur_thres_button.place(relx= 0.55, rely= 0.9,relheight=0.08 , relwidth=0.4 )
 
 image_show = tk.Button(frame3,text='Load the data ' ,command=load)
-image_show.place(relx= 0.4, rely= 0.9,relheight=0.08 , relwidth=0.2 )
+image_show.place(relx= 0.2, rely= 0.9,relheight=0.08 , relwidth=0.2 )
 
-cur_snap = tk.Button(frame3,text='SHOW CURRENT VEHICLE', command= cur_image)
-cur_snap.place(relx= 0.03, rely= 0.9,relheight=0.08 , relwidth=0.33 )
 
 cur_auto_take = tk.Label(frame3,text='VEHICLE IMAGE DATA')
 cur_auto_take.place(relx=0 ,rely= 0.015, relwidth= 1, relheight=0.05)
 #submit button
-def action():
+def database_action():
     car_no = entry_var.get()
     place = place_var.get()
     entry = type_var.get()
@@ -243,30 +308,26 @@ def action():
     if check_var.get()== 1:
         check = 'YES'
     else:
-        check = 'NO' 
-        
+        check = 'NO'
+    with open('img_name.png','rb') as f:
+        image_data = f.read()
+    conn = sqlite3.connect('Toll_data.db')
+    with conn:
+        cursor=conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS Toll(Vehicle_No TEXT,Place TEXT,Entry TEXT,State TEXT,Vehicle_Type TEXT,Date_and_Time TEXT,Donation TEXT,IMAGE)')
+    cursor.execute('INSERT INTO Toll(Vehicle_No,Place,Entry,State,Vehicle_Type,Date_and_Time,Donation,IMAGE) VALUES(?,?,?,?,?,?,?,?)',(car_no,place,entry,state,vehicle,date_time,check,image_data))    
+    conn.commit()
+    conn.close()
     #print(f'{car_no}  {place}  {entry}  {state}  {vehicle}  {date_time}  {check}')
-# write to csv file 
-    with open('file.csv', 'a') as f:
-            dict_writer = DictWriter(f,fieldnames=['VEHICLE NO','PLACE','ENTRY TYPE','STATE','VEHICLE TYPE','DONATION', 'DATE & TIME'])
-            if os.stat('file.csv').st_size==0:
-               dict_writer.writeheader()
-            dict_writer.writerow({
-                    'VEHICLE NO': car_no,
-                    'PLACE': place,
-                    'ENTRY TYPE': entry,
-                    'STATE': state,
-                    'VEHICLE TYPE': vehicle,
-                    'DONATION': check,
-                    'DATE & TIME': date_time
-                    
-                    })
     
 
     vehicle_entry.delete(0,tk.END)
            
-submit_button = tk.Button(frame, text='SUBMIT', command =action)
-submit_button.place(relx= 0.4, rely= 0.9,relheight=0.05 , relwidth=0.2 )
+submit_button = tk.Button(frame, text='SUBMIT', command =database_action)
+submit_button.place(relx= 0.2, rely= 0.9,relheight=0.05 , relwidth=0.2 )
+root.bind("<Return>",database_action)
+snap = tk.Button(frame,text='CAMERA' , command=web)
+snap.place(relx= 0.6, rely= 0.9,relheight=0.05 , relwidth=0.2 )
 
 
 root.mainloop()
